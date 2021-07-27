cddc6a6b539a82e61e715dfc25a5e678e6814811
marcos-inja <marcosvaas@gmail.com>
2021-07-27 20:51:33 -0300

    primeira versao cobrinha

---
+++ cobrinha.py
@@ -0,0 +1,9 @@
+class Cobra:
+    def __init__(self, tam_tela = (300, 400), 
+                        posicao = [100,50], # esquerda, cima
+                        corpo = [[90, 50],[80, 50],[70,50]],
+                        direcao = 'DIREITA'):
+        self.tam_tela = tam_tela
+        self.posicao = posicao
+        self.corpo = corpo
+        self.direcao = direcao
